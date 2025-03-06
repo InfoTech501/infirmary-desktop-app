be70216d102e0dc84796f88ff5f17dd7b6b126f1
kim <kim.yashai.legaspi.panlilio@gmail.com>
2025-03-06 09:46:40 +0800

    FrequentVisitReport Documentation and Revised Database Script.

--- src/main/java/com/rocs/medical/records/application/app/facade/frequentVisitReport/FrequentVisitReportFacade.java
+++ src/main/java/com/rocs/medical/records/application/app/facade/frequentVisitReport/FrequentVisitReportFacade.java
@@ -6,14 +6,26 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * This is an interface that manages the report of the frequent visit of the student in the clinic within the given period of date and grade level
- * * @param startDate
- * *@param endDate
- * *@return list of FrequentVisitReport like student id, student first and last name,grade level, symptoms,visit count, visit date.
+ * * This is a FrequentVisit interface that manages the FrequentVisitReport object such as
+ * * studentId, firstname, lastName, gradeLevel, symptoms, visitCount, and visitDate
+ * * within the given report period.
  * */
-
 public interface FrequentVisitReportFacade {
+    /**
+     * This generates report of the frequent visit of the student in the clinic within the given period of date and grade level
+     * * @param startDate - The start date of the report period.
+     * * @param endDate - The end date of the report period.
+     * * @param gradeLevel - The grade level of the student.
+     * * @return list of FrequentVisitReport object like studentId, firstName, lastName, gradeLevel, symptoms, visitCount, and visitDate.
+     * */
     List<FrequentVisitReport> generateReport(Date startDate, Date endDate, String gradeLevel);
-
+    /**
+     * * This handleFrequentVisit display the report of frequent visit within the given report period.
+     * * @param reports - Checks if there's a report within the given report period.
+     * * @param startDate - The start date of the report period.
+     * * @param endDate - The end date of the report period.
+     * * @param gradeLevel - the grade level of the student.
+     * * @return list of FrequentVisitReport object like studentId, firstName, lastName, gradeLevel, symptoms, visitCount, and visitDate.
+     * */
     void handleFrequentVisit(List<FrequentVisitReport> reports, Date startDate, Date endDate, String gradeLevel);
 }
--- src/main/java/com/rocs/medical/records/application/data/dao/frequentVisitReport/FrequentVisitReportDao.java
+++ src/main/java/com/rocs/medical/records/application/data/dao/frequentVisitReport/FrequentVisitReportDao.java
@@ -6,12 +6,16 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * * This returns the FrequentVisitReport List with the given report period.
- * * @param startDate -  The start of the report date period.
- * * @param endDate - The end of the report date period.
- * * @param gradeLevel - The grade level of the students with frequent visit.
+ * * This is an interface that manages the generated report of FrequentVisitReport object that maintains
+ * * studentId, firstName, lastName, gradeLevel, symptoms, visitCount, and visitDate.
  * */
-
 public interface FrequentVisitReportDao {
+    /**
+     * * This returns the generated report of FrequentVisitReport List with the given report period.
+     * * @param gradeLevel - The grade level of the students with frequent visit.
+     * * @param startDate -  The start of the report date period.
+     * * @param endDate - The end of the report date period.
+     * * @return list of FrequentVisitReport object like studentId, firstName, lastName, gradeLevel, symptoms, visitCount, and visitDate.
+     * */
     List<FrequentVisitReport> getFrequentVisitReports(String gradeLevel, Date startDate, Date endDate);
 }
