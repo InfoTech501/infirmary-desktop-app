fd55e255e72516cde00b32c1ce60f471c6c9da34
juliuspatrickpeji <juliuspatrick.amita.peji@gmail.com>
2025-03-19 09:28:16 +0800

    fixed documentation

--- src/main/java/com/rocs/infirmary/desktop/data/dao/medicine/inventory/MedicineInventoryDao.java
+++ src/main/java/com/rocs/infirmary/desktop/data/dao/medicine/inventory/MedicineInventoryDao.java
@@ -1,13 +1,14 @@
 package com.rocs.infirmary.desktop.data.dao.medicine.inventory;
-import com.rocs.infirmary.desktop.data.model.inventory.medicine.Medicine;
 
+import com.rocs.infirmary.desktop.data.model.inventory.medicine.Medicine;
 import java.util.List;
-public interface MedicineInventoryDao {
 
 
 /*
-* return list of Medicine and inventory objects with details such as medicine name, description, quantity, and expiration date.
-*/
+ * return list of Medicine and inventory objects with details such as medicine name, description, quantity, and expiration date.
+ */
+public interface MedicineInventoryDao {
+
     List<Medicine> getAllMedicine();
 
 }
